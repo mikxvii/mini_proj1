d9a4c30550f4ea6a27ec2a2cf1bf286b738c8c09
Mike Guerrero <mikxvii@berkeley.edu>
2024-02-20 20:55:12 -0800

    changed file paths

--- constants/Constants.js
+++ constants/Constants.js
@@ -1,17 +1,17 @@
 const nameToPic = {
-	riatao: ["Ria Tao", require("../assets/MemberPictures/riatao.jpg")],
-	miladbrown: ["Milad Brown", require("../assets/MemberPictures/miladbrown.jpg")],
-	rianamistry: ["Riana Mistry", require("../assets/MemberPictures/rianamistry.jpg")],
-	landymanzanero: ["Landy Manzanero", require("../assets/MemberPictures/landymanzanero.png")],
-	subhammitra: ["Subham Mitra", require("../assets/MemberPictures/subhammitra.jpeg")],
-	angiezhu: ["Angie Zhu", require("../assets/MemberPictures/angiezhu.png")],
-	edangortzak: ["Edan Gortzak", require("../assets/MemberPictures/edangortzak.jpg")],
-	nadahameed: ["Nada Hameed", require("../assets/MemberPictures/nadahameed.JPG")],
-	morrellnioble: ["Morrell Nioble", require("../assets/MemberPictures/morrellnioble.jpeg")],
-	pavithraarunanand: ["Pavitra Arun Anand", require("../assets/MemberPictures/pavithraarunanand.png")],
-	estherluan: ["Esther Luan", require("../assets/MemberPictures/estherluan.JPG")],
-	danielgil: ["Daniel Gil", require("../assets/MemberPictures/danielgil.jpg")],
-	denniskoh: ["Dennis Koh", require("../assets/MemberPictures/denniskoh.jpeg")],
-	mikeguerrero: ["Mike Guerrero", require("../assets/MemberPictures/mikeguerrero.jpeg")]
+	riatao: ["Ria Tao", require("../assets/MemberPictures/RiaTao.jpg")],
+	miladbrown: ["Milad Brown", require("../assets/MemberPictures/MiladBrown.jpg")],
+	rianamistry: ["Riana Mistry", require("../assets/MemberPictures/RianaMistry.jpg")],
+	landymanzanero: ["Landy Manzanero", require("../assets/MemberPictures/LandyManzanero.png")],
+	subhammitra: ["Subham Mitra", require("../assets/MemberPictures/SubhamMitra.jpeg")],
+	angiezhu: ["Angie Zhu", require("../assets/MemberPictures/AngieZhu.png")],
+	edangortzak: ["Edan Gortzak", require("../assets/MemberPictures/EdanGortzak.jpg")],
+	nadahameed: ["Nada Hameed", require("../assets/MemberPictures/NadaHameed.JPG")],
+	morrellnioble: ["Morrell Nioble", require("../assets/MemberPictures/MorrellNioble.jpeg")],
+	pavithraarunanand: ["Pavitra Arun Anand", require("../assets/MemberPictures/PavithraArunAnand.png")],
+	estherluan: ["Esther Luan", require("../assets/MemberPictures/EstherLuan.JPG")],
+	danielgil: ["Daniel Gil", require("../assets/MemberPictures/DanielGil.jpg")],
+	denniskoh: ["Dennis Koh", require("../assets/MemberPictures/DennisKoh.jpeg")],
+	mikeguerrero: ["Mike Guerrero", require("../assets/MemberPictures/MikeGuerrero.jpeg")]
 }
 export { nameToPic }
